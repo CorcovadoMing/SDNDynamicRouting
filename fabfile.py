8566985e620e824345268b3a6650d46f2d2dc1dc
CorcovadoMing <rf37535@gmail.com>
2015-06-06 23:23:03 +0800

    Fabric wrapper

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from __future__ import with_statement
+from fabric.api import local
+from fabric.colors import green, magenta, yellow
+from fabric.context_managers import hide
+
+def up():
+    with hide('running'):
+        print green(local("docker-compose up -d", capture=True))
+
+def mn():
+    with hide('running'):
+        local("docker-compose run mininet mn --switch user")
+
+def ps():
+    with hide('running'):
+        print yellow(local("docker-compose ps", capture=True))
+
+def rm():
+    with hide('running'):
+        print magenta("[1/2] Stop all service...", bold=True)
+        print green(local("docker-compose stop", capture=True))
+
+        print magenta("[2/2] Remove all container...", bold=True)
+        print green(local("docker-compose rm -f", capture=True))
